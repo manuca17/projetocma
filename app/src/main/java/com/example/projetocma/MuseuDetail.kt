8467b975bed6475acc7c42ff1d28fa740498786c
manuca17 <gokiferreira2@gmail.com>
2023-11-28 21:11:13 +0000

    museum detail page

--- app/src/main/java/com/example/projetocma/MuseuDetail.kt
+++ app/src/main/java/com/example/projetocma/MuseuDetail.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.projetocma.databinding.FragmentMuseuDetailBinding
 
 class MuseuDetail : Fragment() {
 
-
+    private var _binding: FragmentMuseuDetailBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,8 +19,26 @@ class MuseuDetail : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_museu_detail, container, false)
+        _binding = FragmentMuseuDetailBinding.inflate(inflater, container, false)
+        return binding.root
+
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Retrieve arguments
+        val name = arguments?.getString("name")
+        val imageResId = arguments?.getInt("image")
+
+        // Now you can use 'name' and 'imageResId' in your fragment
+
+        // Example: Set the name in a TextView
+        binding.textViewMuseumName.text = name
+
+        // Example: Set the image in an ImageView
+        imageResId?.let { binding.museumImage.setImageResource(it) }
+    }
+
 
 }
--- app/src/main/java/com/example/projetocma/MuseusExplore.kt
+++ app/src/main/java/com/example/projetocma/MuseusExplore.kt
@@ -12,25 +12,36 @@ import models.Museu
 
 class MuseusExplore : Fragment(), Museu.OnItemClickListener {
 
+    val names = arrayOf("asdasdas", "asdasdasdas")
+    val img = arrayOf(R.drawable.imageplaceholder, R.drawable.imageplaceholder).toIntArray()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_museus_explore, container, false)
 
-        val names = arrayOf("asdasdas", "asdasdasdas")
-        val img = arrayOf(R.drawable.imageplaceholder, R.drawable.imageplaceholder).toIntArray()
+
 
         val museuAdapter = Museu(requireContext(), names, img, this)
 
         val gridView: GridView = view.findViewById(R.id.gridView)
         gridView.adapter = museuAdapter
 
+
         return view
     }
 
     override fun onItemClick(position: Int) {
-        // Handle item click, for example, navigate to a new fragment with details
-        findNavController().navigate(R.id.museuDetail)
+        val selectedName = names[position]
+        val selectedImage = img[position]
+
+        val bundle = Bundle()
+        bundle.putString("name", selectedName)
+        bundle.putInt("image", selectedImage)
+
+        findNavController().navigate(R.id.museuDetail, bundle)
+
+
     }
 }
